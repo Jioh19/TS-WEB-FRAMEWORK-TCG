7f6c24c276cfec5ec1323fba6eebe05828ab1c2c
Jioh19 <jioh1982@gmail.com>
2023-10-10 23:27:34 -0300

    add user.fetch

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,9 @@
 import { User } from './models/User';
 
-const user = new User({ name: 'myname', age: 20 });
-user.set({ name: 'newname' });
-user.on('change', () => {
-	console.log('Change #1');
-});
-user.on('click', () => {
-	console.log('Change #2');
-});
-user.on('save', () => {
-	console.log('Change #3');
-});
+const user = new User({ id: 1 });
 
-user.trigger('change');
-user.trigger('click');
+user.fetch();
+
+setTimeout(() => {
+	console.log(user);
+}, 4000);
